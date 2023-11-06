cb9b9254597d73745dc43d4408002c63d5d459e0
gabriela tururu <gabriela.aguayaire@gmail.com>
2023-11-05 18:20:30 -0600

    FIRST COMMIT_HabitsTrackerDashboard

---
+++ app.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('#addButton')
+// button.addEventListener('click',()=>{
+
+//     const div = document.createElement('div')
+//     div.classList.add('container')
+//     div.innerHTML = `
+//         <div class=''>
+//     `
+
+// })
